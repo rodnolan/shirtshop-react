14a557165dcc58c8fc9f02a1476fed30bf7cc068
Shanta Nathwani <shanta@shanta.ca>
2017-11-15 12:47:36 -0500

    Added base html to the app as wireframe

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+          <div id="wrapper">Wrapper
+            <div id="shirtlist">shirtlist</div>
+            <div id="content">content
+              <div id="shopbar">shopbar</div>
+              <div id="shirt">shirt</div>
+              <div id="picker">picker</div>
+              <div id="statusbar">statusbar</div>
+          </div>
+          <div id="configbar">configbar</div>
+        </div>
       </div>
     );
   }
