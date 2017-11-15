a14292a2047e3daa354cf0b0979eaec1b26a0213
Rod Nolan <rodnolan@gmail.com>
2017-11-15 16:28:33 -0500

    first commit for two simple components

---
+++ src/components/Shirt/Shirt.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Shirt.css';
+
+class Shirt extends Component {
+  render() {
+    return (
+      <p className="Shirt">shirt</p>
+    );
+  }
+}
+
+export default Shirt;
---
+++ src/components/ShirtList/ShirtList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './ShirtList.css';
+
+class ShirtList extends Component {
+    render() {
+      return (
+        <p className="ShirtList">shirt list</p>
+      );
+    }
+}
+
+export default ShirtList;
