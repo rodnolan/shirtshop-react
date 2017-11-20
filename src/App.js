91fcd877fff21dc9c2e66699b8767ae6f520820b
Rod Nolan <rodnolan@gmail.com>
2017-11-20 16:16:28 -0500

    first crack at navigation

--- src/App.js
+++ src/App.js
@@ -1,53 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      {/* Wrapper became container for Bootstrap purposes. */}
         <div className ="container-fluid">
           <nav className="navbar">
-            navbar
+            <Link to="config">Config</Link>
+            <Link to="cart">Cart</Link>
           </nav>
-          <div className="row main-column">
-              <div className = "col-xs-12 col-sm-3 col-md-3" classID ="statuslist">
-                <div className="row">
-                  <div className="col-12" classID="status-color">status-color</div>                 
-                </div>
-                <div className="row">
-                  <div className="col-12" classID="status-style">status-style</div>
-                </div>
-                <div className="row">
-                  <div className="col-12" classID="status-size">status-size</div>
-                </div>
-                <div className="row">
-                  <div className="col-12" classID="status-cost">status-cost</div>
-                </div>
-                  {/* <button type="button" className="btn btn-block" classID="shirt-list">Shirt1,etc.</button> */}
-              </div>
-              <div className = "col-xs-12 col-sm-6 col-md-6" classID ="content">
-                  <div className = "row content-row" classID ="shopbar">
-                    <button type="button" className="btn btn-md col-4" classID="shop-save">Save</button>
-                    <button type="button" className="btn btn-md col-4" classID="shop-cancel">Cancel</button>
-                    <button type="button" className="btn btn-md col-4" classID="shop-cart">Cart</button>
-                  </div>
-                  <div className = "row shirt-row" classID ="shirt">shirt area
-                  </div>
-                  {/* <div className = "row content-row" classID ="picker">
-                    <button type="button" className="btn btn-block" classID="btn-picker">Dynamic Buttons</button>
-                  </div> */}
-                  {/* <div className = "row content-row" classID ="statusbar">
-
-                  </div> */}
-              </div>
-            <div className = "col-xs-12 col-sm-3 col-md-3" classID="configbar">
-              <button type="button" className="btn btn-block" classID="config-color">Color</button>
-              <button type="button" className="btn btn-block" classID="config-style">Style</button>
-              <button type="button" className="btn btn-block" classID="config-size">Size</button>
-              <button type="button" className="btn btn-block" classID="config-caption">Caption</button>
-            </div>
-          </div>
+          {this.props.children}
         </div> 
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+//import { Router } from 'react-router';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ShirtList from './components/ShirtList/ShirtList';
+import Config from './components/Config/Config';
+import Cart from './components/Cart/Cart';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Route path="/" component={App}>
+            <Route path="/" component={ShirtList} />
+            <Route path="config" component={Config} />
+            <Route path="cart" component={Cart} />
+        </Route>
+    </BrowserRouter>, document.getElementById('root')
+);
+
 registerServiceWorker();
+
+
