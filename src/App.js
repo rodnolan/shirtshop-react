cb47100ec38238d3d7ac5f8c096edd464d7e3cf6
Shanta Nathwani <shanta@shanta.ca>
2017-11-20 13:36:43 -0500

    Fixed bootstrap layout to suit version 4 better. Still not perfect but cleaner code and better responsiveness.

--- src/App.js
+++ src/App.js
@@ -10,27 +10,21 @@ class App extends Component {
           <nav className="navbar">
             navbar
           </nav>
-          <div className="row main-column">
-              <div className = "col-xs-12 col-sm-3 col-md-3" classID ="statuslist">
+          <div className="main-column row">
+              <div className = "col" classID ="statuslist">
                 <div className="row">
-                  <div className="col-12" classID="status-color">status-color</div>                 
-                </div>
-                <div className="row">
-                  <div className="col-12" classID="status-style">status-style</div>
-                </div>
-                <div className="row">
-                  <div className="col-12" classID="status-size">status-size</div>
-                </div>
-                <div className="row">
-                  <div className="col-12" classID="status-cost">status-cost</div>
+                  <div className="col" classID="status-color">status-color</div>
+                  <div className="col" classID="status-style">status-style</div>
+                  <div className="col" classID="status-size">status-size</div>
+                  <div className="col" classID="status-cost">status-cost</div>
                 </div>
                   {/* <button type="button" className="btn btn-block" classID="shirt-list">Shirt1,etc.</button> */}
               </div>
-              <div className = "col-xs-12 col-sm-6 col-md-6" classID ="content">
+              <div className = "col-6" classID ="content">
                   <div className = "row content-row" classID ="shopbar">
-                    <button type="button" className="btn btn-md col-4" classID="shop-save">Save</button>
-                    <button type="button" className="btn btn-md col-4" classID="shop-cancel">Cancel</button>
-                    <button type="button" className="btn btn-md col-4" classID="shop-cart">Cart</button>
+                    <button type="button" className="btn col" classID="shop-save">Save</button>
+                    <button type="button" className="btn col" classID="shop-cancel">Cancel</button>
+                    <button type="button" className="btn col" classID="shop-cart">Cart</button>
                   </div>
                   <div className = "row shirt-row" classID ="shirt">shirt area
                   </div>
@@ -41,11 +35,21 @@ class App extends Component {
 
                   </div> */}
               </div>
-            <div className = "col-xs-12 col-sm-3 col-md-3" classID="configbar">
-              <button type="button" className="btn btn-block" classID="config-color">Color</button>
-              <button type="button" className="btn btn-block" classID="config-style">Style</button>
-              <button type="button" className="btn btn-block" classID="config-size">Size</button>
-              <button type="button" className="btn btn-block" classID="config-caption">Caption</button>
+            <div className = "col" classID="configbar">
+              <div className="row">
+                <div className = "col">
+                  <button type="button" className="btn btn-block" classID="config-color">Color</button>
+                </div>
+                <div className = "col">
+                  <button type="button" className="btn btn-block" classID="config-style">Style</button>
+                </div>
+                <div className = "col">
+                  <button type="button" className="btn btn-block" classID="config-size">Size</button>
+                </div>
+                <div className = "col">
+                  <button type="button" className="btn btn-block" classID="config-caption">Caption</button>            
+                </div>
+              </div>
             </div>
           </div>
         </div> 
