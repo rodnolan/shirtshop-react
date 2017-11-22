804f931e073b1c83f4ec193b0d77495206abf37a
Shanta Nathwani <shanta@shanta.ca>
2017-11-22 14:00:55 -0500

    Implemented icons on the config page. Embedded font-awesome on index.html

--- src/components/Config/Config.js
+++ src/components/Config/Config.js
@@ -17,17 +17,18 @@ class Config extends Component {
           </div>
           <div className = "col-12 col-sm-12 col-md-6" classID ="content">
             <div className = "row content-row" classID ="shopbar">
-              <button type="button" className="btn btn-md col-4" classID="shop-save">Save</button>
-              <button type="button" className="btn btn-md col-4" classID="shop-cancel">Cancel</button>
-              <button type="button" className="btn btn-md col-4" classID="shop-cart">Cart</button>
+              <button type="button" className="btn btn-md col-4" classID="shop-save"><i class="fa fa-floppy-o" aria-hidden="true">Save</i></button>
+              {/* Alternate icon for save: <i class="fa fa-check" aria-hidden="true"></i> */}
+              <button type="button" className="btn btn-md col-4" classID="shop-cancel"><i class="fa fa-ban" aria-hidden="true">Cancel</i></button>
+              <button type="button" className="btn btn-md col-4" classID="shop-cart"><i class="fa fa-cart-plus" aria-hidden="true">Cart</i></button>
             </div>
             <div className = "row shirt-row" classID ="shirt">shirt area</div>
           </div>
           <div className = "col-12 col-sm-12 col-md-3" classID="configbar">
-            <button type="button" className="col-3 col-sm-3 col-md-12 btn-md" classID="config-color">Color</button>
-            <button type="button" className="col-3 col-sm-3 col-md-12 btn-md" classID="config-style">Style</button>
-            <button type="button" className="col-3 col-sm-3 col-md-12 btn-md" classID="config-size">Size</button>
-            <button type="button" className="col-3 col-sm-3 col-md-12 btn-md" classID="config-caption">Caption</button>
+            <button type="button" className="col-3 col-sm-3 col-md-12 btn-md" classID="config-color"><i class="fa fa-adjust" aria-hidden="true"></i>Color</button>
+            <button type="button" className="col-3 col-sm-3 col-md-12 btn-md" classID="config-style"><i class="fa fa-shirtsinbulk" aria-hidden="true"></i>Style</button>
+            <button type="button" className="col-3 col-sm-3 col-md-12 btn-md" classID="config-size"><i class="fa fa-arrows-v" aria-hidden="true"></i>Size</button>
+            <button type="button" className="col-3 col-sm-3 col-md-12 btn-md" classID="config-caption"><i class="fa fa-font" aria-hidden="true"></i>Caption</button>
           </div>
         </div>
       </div>
