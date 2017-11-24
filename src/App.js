83febe8541d5f3f148057b23efc463f3295cebc9
Rod Nolan <rodnolan@gmail.com>
2017-11-24 16:06:12 -0500

    moving storage to App.js from the ShirtList component

--- src/App.js
+++ src/App.js
@@ -9,15 +9,47 @@ import ShirtList from './components/ShirtList/ShirtList';
 import Cart from './components/Cart/Cart';
 import Config from './components/Config/Config';
 import NavBar from './components/NavBar/NavBar';
+import store from 'store';
 
 class ShirtShop extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      shirts: []
+    }
+  }
+
+  componentDidMount() {
+    console.log('App::componentDidMount');
+    // this should trigger a re-render because it's being called from component *DID* Mount, not componentWillMount
+    this.createSampleData();
+  }
+  
+  createSampleData = () => {
+    console.log('App::createSampleData');
+    let sampleData = [
+      {id: 1, price: '10', size: 'S', color: 'red', style: 'mens', caption: 'my shirt one'},
+      {id: 2, price: '11', size: 'M', color: 'blue', style: 'womens', caption: 'my shirt two'},
+      {id: 3, price: '12', size: 'L', color: 'white', style: 'womens', caption: 'my shirt three'},
+      {id: 4, price: '12', size: 'L', color: 'black', style: 'mens', caption: 'my shirt four'},
+      {id: 5, price: '10', size: 'S', color: 'black', style: 'womens', caption: 'my shirt five'}
+    ];
+    store.set('shirts', sampleData);
+    let storedShirts = store.get('shirts');
+    this.setState({ shirts: storedShirts});
+    console.log(storedShirts.length + ' shirts found in storage');
+  };
+
+
   render() {
+    console.log('App::render');
     return (
       <Router>
         <div>
           <NavBar />
           <hr/>
-          <Route exact path="/" component={ShirtList}/>
+          {/* <Route exact path="/" component={ShirtList}/> */}
+          <Route exact path="/" render={() => <ShirtList shirts={this.state.shirts} />} />
           <Route path="/config" component={Config}/>
           <Route path="/cart" component={Cart}/>
         </div>
--- src/components/ShirtList/ShirtList.js
+++ src/components/ShirtList/ShirtList.js
@@ -1,62 +1,23 @@
 import React from 'react';
 import ShirtListRow from './ShirtListRow';
 import './ShirtList.css';
-import store from 'store';
+
 
 class ShirtList extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      shirts: []
-    }
-  }
-
-  componentWillMount = () => {
-    let storedShirts = store.get('shirts') || [];
-    console.log(storedShirts.length + ' shirts found in storage');
-    this.setState({shirts: storedShirts})
-  };
-
-  // temporary function used to create some sample data; building the UI is easier when there's real data to look at
-  createSampleData = () => {
-    let sampleData = [
-        {id: 1, price: '10', size: 'S', color: 'red', style: 'mens', caption: 'my shirt one'},
-        {id: 2, price: '11', size: 'M', color: 'blue', style: 'womens', caption: 'my shirt two'},
-        {id: 3, price: '12', size: 'L', color: 'white', style: 'womens', caption: 'my shirt three'},
-        {id: 4, price: '12', size: 'L', color: 'black', style: 'mens', caption: 'my shirt four'}
-    ];
-    store.set('shirts', sampleData);
-    let storedShirts = store.get('shirts');
-    this.setState({ shirts: storedShirts});
-  };
-
-  removeAllShirts = () => {
-    store.remove('shirts');
-    this.setState({ shirts: []});
-  };
-
     render = (props) => (
         <div>
             <p className="ShirtList">shirt list</p>
-            { this.state.shirts.length === 0 ? this.renderAddButton() : this.renderShirts() }
+            { this.props.shirts.length === 0 ? null : this.renderShirts() }
         </div>
     )
-    renderAddButton() {
-        return (
-            <div>
-                <p>you have no shirts saved</p>
-                <button onClick={this.createSampleData}>create some sample shirts</button>
-            </div>
-        )
-    }
+
     renderShirts() {
         return (
             <div>
             <p>Here are your saved shirts</p>
             <ul>
-                { this.state.shirts.map(this.renderShirtItem) }
+                { this.props.shirts.map(this.renderShirtItem) }
             </ul>
-            <button onClick={this.removeAllShirts}>Clear All</button>
             </div>
         )
     }
