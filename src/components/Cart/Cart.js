e714c0355e26656a232881e57ea03a565d3efc9e
Shanta Nathwani <shanta@shanta.ca>
2017-11-29 12:37:00 -0500

    Updated individual inputs to update state appropriately.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,18 @@
 // Just a thought: Perhaps this "page" should be broken up into two separate components.
 import React, { Component } from 'react';
 import './Cart.css';
-import regions from './regions' //This doesn't seem to want to import. 
+import countries from './countries'; 
 //import ShirtListRow from './../ShirtListRow'; //For some reason I'm getting an error that it cannot find this component when I try to import it.
 
 class Cart extends Component {
   constructor(){
     super();
     this.completeOrder = this.completeOrder.bind(this);
-    this.shippingInfo = this.shippingInfo.bind(this);
+    this.updateShippingInfo = this.updateShippingInfo.bind(this);
 
     // Set the initial state
     this.state = {
-      testname: '',
+      country: "usa",
       // The cart items are yet to be completed orders and will be brought down from the App itself once the storage piece is worked out. The orders are going to represent completed orders that are sent to the storage.
       orders: {},
       cartItems: [
@@ -25,20 +25,18 @@ class Cart extends Component {
 
       currentCartItems: [],
       // cartItems: this.props.cartItems, //This doesn't work either through the router component. Returns undefined. Part of GitHub Issue #20
-      
-      //Imports the regions for the form, including countries, provinces and states (called "unitedstates" to avoid confusion. This is good. Don't touch this.)
-      regions: regions,
+      countries: countries
 
       //Temporary storage for testing purposes. Triggered by the onclick event of the ship it button.
-      shippingInfo: {}
+      // shippingData: {}
       
     }  
   }
 
-  //This intermediary data will take the items from the App.js cartItems state and assign it to a temporary item here. GitHub Issue #18
-  componentWillMount(){
-    this.setState({currentCartItems: this.state.cartItems});
-  }
+  //This intermediary data will take the items from the App.js cartItems state and assign it to a temporary item here. GitHub Issue #18. UPDATE November 29: This may not be needed at all. Going to read directly from App.state.
+  // componentWillMount(){
+  //   this.setState({currentCartItems: this.state.cartItems});
+  // }
 
   // This function will take the items in the cart (the quantity, etc.), the form submission and send it to the storage. GitHub Issue #14
   completeOrder(){
@@ -49,14 +47,19 @@ class Cart extends Component {
     //  3. the shipping details, including when the order was completed, perhaps by formatted date stamp
   }
 
-  shippingInfo = (event) => {
-    this.setState({testname: event.target.value});
-      //this.setState({shippingInfo: blah}): //form stuff.  Github Issue #14
-    console.log(this.state.testname);
+  //form stuff.  Github Issue #14 - This currently works for a single field. Going 
+  updateShippingInfo = (event) => {
+    let field = event.target.id;
+    let val = event.target.value;
+    this.setState({[field]: val});
+    console.log('Field: ' + field);
+    console.log('Value: ' + val);
   }
 
   render() {
     // const {cartItems} = this.props; <-- Was attempting to get the props, but there is an error having to do with the router.
+    let regions = this.state.countries[this.state.country].regions;
+
     return (
       <div className="row">
         <div className="col">
@@ -123,53 +126,53 @@ class Cart extends Component {
           </table>
           <h2 classID="shipHeader">Shipping Details</h2>
           <form className="form-ship form-horizontal"> {/* form-horizontal from Bootstrap is a decent choice, provided that we add some padding both on the inside of the form container, as well as on the elements. Maybe on the .form-control, which is applied to each of the form elements*/}
-              <div className="form-group">
-                <label htmlFor="firstName">First Name: </label><input type="text" value={this.state.testname} onChange={this.shippingInfo} className="form-control col-form-label col-4" classID="firstName"  placeholder="First Name" />
+              <div className="form-group col">
+                <label htmlFor="firstName">First Name: </label><input type="text" value={this.state[this.id]} onChange={this.updateShippingInfo} className="form-control col-form-label col-4" id="firstName"  placeholder="First Name" />
               </div>
               <div className="form-group">
-                <label htmlFor="lastName">Last Name: </label><input type="text" className="form-control col-form-label col-4" classID="lastName"  placeholder="Last Name" />
+                <label htmlFor="lastName">Last Name: </label><input type="text" value={this.state[this.id]} onChange={this.updateShippingInfo} className="form-control col-form-label col-4" id="lastName"  placeholder="Last Name" />
               </div>
             <div className="form-group">
-              <label htmlFor="email">Email address</label><input type="email" className="form-control col-form-label" classID="email"  placeholder="Email" />
+              <label htmlFor="email">Email address</label><input type="email" value={this.state[this.id]} onChange={this.updateShippingInfo} className="form-control col-form-label" id="email"  placeholder="Email" />
             </div>
             <div className="form-group">
-              <label htmlFor="address">Address</label><input type="text" className="form-control col-form-label" classID="cddress"  placeholder="Street Address" />
+              <label htmlFor="address">Address</label><input type="text" value={this.state[this.id]} onChange={this.updateShippingInfo} className="form-control col-form-label" id="cddress"  placeholder="Street Address" />
             </div>
             <div className="form-group">
-                <label htmlFor="city"></label>City<input type="text" className="form-control col-form-label col-6" classID="city"  placeholder="City" />
+                <label htmlFor="city"></label>City<input type="text" value={this.state[this.id]} onChange={this.updateShippingInfo} className="form-control col-form-label col-6" id="city"  placeholder="City" />
             </div>
             <div className="form-group">
               <label htmlFor="country">Country</label>
-              {/* <input type="text" className="form-control col-form-label" classID="country"  placeholder="Country" /> */}
-              <select className="form-control col-5"  classID="countrySelector"> 
-                {/* <option selected value="default">Select Your Country</option> */} {/* This should be the default & disabled, but it stops the form from working. */}
-                <option value={this.state.regions.countries.canada}>{this.state.regions.countries.canada}</option>
-                <option value={this.state.regions.countries.usa}>{this.state.regions.countries.usa}</option>
+              {/* <input type="text" className="form-control col-form-label" id="country"  placeholder="Country" /> */}
+              <select className="form-control col-5"  value={this.state[this.id]} onChange={this.updateShippingInfo} id="country"> 
+              {
+                Object.keys(this.state.countries)
+                  .map((country, i) => 
+                      (
+                        <option value={country.name}>{country.name}</option>
+                      )
+                  )
+              }
+
               </select>
+
               <label htmlFor="provState">Province or State</label> {/* This entire list needs to change based on the conditional. For now, I've just put in the provinces. */}
-              <select className="form-control col-5" classID="provinceSelector"> 
+              <select className="form-control col-5" value={this.state[this.id]} onChange={this.updateShippingInfo} id="province"> 
                 {/* Currently gives Ontario only */}
                 {/* <option>{this.state.provinces.on}</option> */}
                 {/* <option>Select Your Province</option> */}
-                {Object.keys(this.state.regions.provinces).map((province, i) => (
-                    <option>{this.state.regions.provinces[province]}</option>
-                  )
-                )}
-              </select>
-              <select className="form-control col-5" classID="unitedStateSelector"> {/* Need to have this autogenerate*/}
-                {/* <option value="" >Select Your State</option> */}
-                {Object.keys(this.state.regions.unitedstates).map((unitedstate, i) => (
-                    <option>{this.state.regions.unitedstates[unitedstate]}</option>
+                {Object.keys(regions).map((region, i) => (
+                    <option>{region}</option>
                   )
                 )}
               </select>
             </div>
             <div className="form-group">
               {/* The postal or zip code needs to be conditional upon the country to determine postal vs. zip */}
-              <label htmlFor="postZip">Postal or Zip Code</label><input type="text" className="form-control col-form-label col-4" classID="postZip"  placeholder="Postal or Zip Code" />
+              <label htmlFor="postZip">Postal or Zip Code</label><input type="text" value={this.state[this.id]} onChange={this.updateShippingInfo} className="form-control col-form-label col-4" id="postZip"  placeholder="Postal or Zip Code" />
             </div>
             <div className="form-group">
-              <label htmlFor="specialInstructions">Special Instructions</label><textarea className="form-control col-form-label" classID="specialInstructions" rows="3" placeholder="Anything we should know about?"></textarea>
+              <label htmlFor="specialInstructions">Special Instructions</label><textarea value={this.state[this.id]} onChange={this.updateShippingInfo} className="form-control col-form-label" id="specialInstructions" rows="3" placeholder="Anything we should know about?"></textarea>
               <button type="submit" className="btn btn-primary" >Ship It!</button>
             </div>
           </form>
