106894fcb077e94c5cd093659cd7f9eca5b9958d
Shanta Nathwani <shanta@shanta.ca>
2017-12-04 09:43:35 -0500

    Fixed rendershirt in ShirtList.js

--- src/components/ShirtList/ShirtList.js
+++ src/components/ShirtList/ShirtList.js
@@ -3,19 +3,26 @@ import ShirtListRow from './ShirtListRow';
 import './ShirtList.css';
 
 class ShirtList extends React.Component {
+  constructor() {
+    super();
+    this.renderShirtItem = this.renderShirtItem.bind(this);
+  }
   render = props => (
     <div>{this.props.shirts.length === 0 ? null : this.renderShirts()}</div>
   );
 
   renderShirts() {
-    return <div>{this.props.shirts.map(this.renderShirtItem)}</div>;
+    return (
+      <div>{Object.keys(this.props.shirts).map(this.renderShirtItem)}</div>
+    );
   }
 
-  renderShirtItem(item) {
+  renderShirtItem(key) {
+    let shirt = this.props.shirts[key];
     return (
-      <div key={item.id}>
+      <div key={shirt.id}>
         {' '}
-        <ShirtListRow shirt={item} />
+        <ShirtListRow shirt={shirt} />
       </div>
     );
   }
