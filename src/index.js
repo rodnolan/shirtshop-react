2440389846395daecf0d11e7fddd46f156a1f038
Rod Nolan <rodnolan@gmail.com>
2017-12-07 14:59:58 -0500

    removing carousel junk

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './carousel.css';
 import ShirtShop from './App';
 import registerServiceWorker from './registerServiceWorker';
 
